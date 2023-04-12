eacb8c55062cf25a41c0e049c14d0e809cc68e7e
Albert Veyson <veyson@bk.ru>
2023-04-12 16:04:31 +0500

    config

--- config/filesystems.php
+++ config/filesystems.php
@@ -43,6 +43,13 @@ return [
             'visibility' => 'public',
             'throw' => false,
         ],
+        'avatars' => [
+            'driver' => 'local',
+            'root' => storage_path('app/avatars'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'avatars',
+            'throw' => true,
+        ],
 
         's3' => [
             'driver' => 's3',
